605da0bda7a12b91a6640ab9b3639135e7370b93
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-02 16:51:11 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//users need to be able to:
+
+//enter items they need to purchase 
+//by entering text and hitting "Return" or clicking the "Add item" button
+
+//check and uncheck items on the list by clicking the "Check" button
+
+//permanently remove items from the list
+
+//Hint: you may find it helpful to read up on and use the following 
+//jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
