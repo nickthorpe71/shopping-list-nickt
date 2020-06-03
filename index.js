61e377b1950b9c53bad55f83e3f09b34e862bcdf
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-02 21:17:54 -0500

    All checkpoints complete

--- index.js
+++ index.js
@@ -1,11 +1,93 @@
-//users need to be able to:
+'use strict';
 
-//enter items they need to purchase 
-//by entering text and hitting "Return" or clicking the "Add item" button
+/**
+ * Ensures button state matches item state
+ */
+function correctState() {
+  let list = $('.shopping-item');
 
-//check and uncheck items on the list by clicking the "Check" button
+  $.each(list, function (index, item) {
+    let buttonLabel = $(item).parent().find('.shopping-item-toggle .button-label');
+    if ($(item).hasClass('shopping-item__checked')) {
+      buttonLabel.text('uncheck');
+    } else {
+      buttonLabel.text('check');
+    }
+  });
+}
 
-//permanently remove items from the list
+function handleUserInput() {
 
-//Hint: you may find it helpful to read up on and use the following 
-//jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
+  //set up a listener on the input field of the add item form
+  $('#js-shopping-list-form input').on('keypress', function (event) {
+    //if user enters text and hits return
+    if (event.which === 13) {
+      event.preventDefault();
+      //create a new shopping item at the bottom of the list
+      crateNewItem(this.value);
+    }
+  });
+
+  //set up a listener on the add item form
+  //if user clicks the add item button
+  $('#js-shopping-list-form').submit(function (event) {
+    event.preventDefault();
+    //create a new shopping item at the bottom of the list
+    crateNewItem($('input').first().val());
+  });
+
+  //add a listener to the .shopping-list class
+  $('.shopping-list').on('click', 'button', function () {
+    //check which button was pressed
+    let buttonLabel = $(this).find('.button-label');
+    //check the list item name
+    let item = $(this).closest('li').find('.shopping-item');
+    //if delete was pressed
+    if (buttonLabel.text() === 'delete') {
+      deleteItem(item);
+    }
+    //if delete was pressed
+    else if (buttonLabel.text() === 'check' || buttonLabel.text() === 'uncheck') {
+      toggleItem(item, buttonLabel);
+    }
+  });
+}
+
+function toggleItem(item, buttonLabel) {
+  if (item.css('text-decoration').indexOf('line-through') >= 0) {
+    $(item.css('text-decoration', 'none'));
+    buttonLabel.text('check');
+  }
+  else {
+    $(item.css('text-decoration', 'line-through'));
+    buttonLabel.text('uncheck');
+  }
+  $(item.toggleClass('shopping-item__checked'));
+}
+
+function deleteItem(item) {
+  item.parent().remove();
+}
+
+function crateNewItem(item) {
+  console.log(`Creating item: ${item}`);
+
+  $('.shopping-list').append(
+    `<li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+  );
+}
+
+$(function () {
+  correctState();
+  handleUserInput();
+});
